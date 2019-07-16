4c637c44bfa7aad2b6417f165c587fe690db41ad
crisgon123 <crisgon123@gmail.com>
2019-07-15 22:02:39 -0500

    15/07/2019

---
+++ Semana8/Dia1/clases/clases/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Celular miLadrillo = new Celular("NOKIA", "8110", "Celeste", "941090977","89098783959454");
+            miLadrillo.llamar("956948745");
+           
+            Celular miCel = new Celular("NOKIA", "8010", "Negro", "941090977", "89098783959454");
+            miLadrillo.llamar("956948745");
+
+            miLadrillo.Colgar();
+            Console.WriteLine($"Has credo {Celular.numeroDeCelulares } celulares");
+            Console.WriteLine($"Mi numero es {miLadrillo.Numero}");
+            Console.WriteLine($"Mi otro numero es {miCel.Numero}, marca {miCel.GetMarca()}");
+            miCel.SetColor("Rojo");
+        }
+    }
+    class Celular
+        {
+            string marca;
+            string modelo;
+            string color;
+            string numero;
+            string IMEI;
+        //ATRIBUTO DE CLASE ESTATICO
+        //es un atributo propio de la classe por eso static
+        public static int numeroDeCelulares = 0;
+
+
+        //control+. encapsular y seguir usandolo
+        public string Marca { get => marca; set => marca = value; }
+        public string Marca1 { get => marca; set => marca = value; }
+        public string Modelo { get => modelo; set => modelo = value; }
+        public string Color { get => color; set => color = value; }
+        public string Numero { get => numero; set => numero = value; }
+        public string IMEI1 { get => IMEI; set => IMEI = value; }
+
+        // GET y SET
+        // obtener y establecer, poner
+
+
+        public String GetMarca()
+        {
+            return marca;
+
+        }
+        public String GetColor()
+        {
+            return color;
+
+        }
+        public void SetColor(string color)
+        {
+            this.color = color;
+        }
+        public Celular(string marca, string modelo, string color, string numero, string iMEI)
+            {
+                this.marca = marca;
+                this.modelo = modelo;
+                this.color = color;
+                this.numero = numero;
+                IMEI = iMEI;
+                numeroDeCelulares++;
+            }
+
+            public void llamar(string numero)
+            {
+
+            }
+            public int Colgar()
+            {
+                return (0);
+            }
+       
+    }
+}
---
+++ Semana8/Dia1/cuenta/cuenta/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace cuenta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cuenta miCuenta = new Cuenta("Ronald",25);
+            Cuenta miCuenta1 = new Cuenta("Carlos");
+            Console.WriteLine($" la cantidad del ususario {miCuenta.Titular} es:  {miCuenta.Cantidad}");
+            Console.WriteLine($" la cantidad del ususario {miCuenta1.Titular} es:  {miCuenta1.Cantidad}");
+            Console.WriteLine("**********************************************");
+            miCuenta.Retirar(24);
+            Console.WriteLine($" la cantidad restante del usuario {miCuenta.Titular} es: {miCuenta.Cantidad}");
+            Console.WriteLine("**********************************************");
+            miCuenta.Ingresar(-32);
+            Console.WriteLine($" la cantidad del ususario {miCuenta.Titular} es: {miCuenta.Cantidad}");
+        }
+    }
+    class Cuenta
+    {
+        string titular;
+        double cantidad;
+
+        public Cuenta(string titular, double cantidad=0)
+        {
+            this.Titular = titular;
+            this.Cantidad = cantidad;
+        }
+
+        public string Titular { get => titular; set => titular = value; }
+        public double Cantidad { get => cantidad; set => cantidad = value; }
+
+        public  void Ingresar(double cantidad)
+        {
+            if (cantidad > 0)
+                this.Cantidad = Cantidad+cantidad;
+            else
+                Console.WriteLine("La cantidad no puede ser negativa");
+            
+        }
+        public void Retirar(double cantidad)
+        {
+            if((Cantidad-cantidad)<=0)
+            this.Cantidad = 0;
+            else
+            this.Cantidad = Cantidad - cantidad;
+        }
+    }
+}
