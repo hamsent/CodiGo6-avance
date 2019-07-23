b388b47ff246807d4702ce3aa243a7f908a589dd
hamsent <r.huarca.toledo@gmail.com>
2019-07-23 16:33:04 -0500

    23/07/2019 C

---
+++ Semana9/Dia2/C#/ejercicio/ejercicio/Pelicula.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ejercicio
+{
+    class Pelicula
+    {
+        int id;
+        string titulo;
+        string genero;
+        double precio;
+        string disponibilidad;
+        string puntuacion;
+        int estreno;
+
+       
+        public Pelicula(int id, string titulo, string genero, double precio, string disponibilidad, string puntuacion, int estreno)
+        {
+            this.id = id;
+            this.titulo = titulo;
+            this.genero = genero;
+            this.precio = precio;
+            this.disponibilidad = disponibilidad;
+            this.puntuacion = puntuacion;
+            this.estreno = estreno;
+        }
+
+        public Pelicula()
+        {
+            this.id = 1;
+            this.titulo = "Corazón Valiente";
+            this.genero = "Drama";
+            this.precio = 10;
+            this.disponibilidad = "no";
+            this.puntuacion = "5 estrellas";
+            this.estreno = 2002;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Titulo { get => titulo; set => titulo = value; }
+        public string Genero { get => genero; set => genero = value; }
+        public double Precio { get => precio; set => precio = value; }
+        public string Disponibilidad { get => disponibilidad; set => disponibilidad = value; }
+        public string Puntuacion { get => puntuacion; set => puntuacion = value; }
+        public int Estreno { get => estreno; set => estreno = value; }
+
+
+        //List<Pelicula> lista = new List<Pelicula>();
+        //public void Guardar(Pelicula modelo)
+        //{
+        //    lista.Add(modelo);
+        //}
+        //public List<Pelicula> Consultar()
+        //{
+        //    return lista;
+        //}
+        public void peliculaDisponible()
+        {
+            if (this.Disponibilidad == "si")
+            {
+                Console.WriteLine("TITULO: "+this.Titulo);
+            }
+            else
+            {
+                Console.WriteLine("no hay peliculas disponibles");
+            }
+        }
+
+        public void verificarDisponibilidad()
+        {
+
+        }
+    }
+}
---
+++ Semana9/Dia2/C#/ejercicio/ejercicio/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+
+namespace ejercicio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ejecutar();
+            //            Control de ventas de películas
+            //1Hacer un menu
+
+            //    int opcion
+            //    (do -while)
+            //    1 ver peliculas disponibles
+            //    2 verificar disponibilidad
+            //    3 comprar pelicula
+            //    4 devolver película
+            //    5 peliculas populares
+            //2 Crear una clase Pelicula
+            //2.1 Atributos: id, titulo, precio, disponibilidad, puntuacion
+            //3 Arreglo de películas
+            //4 Funciones:
+            //            4.1 - 2 Foreach de peliculas con disponibilidad
+            //4.3 Reducir stock
+            //4.4 Aumenta stock
+            //4.5 Ordenamiento por puntuacion
+            Pelicula peli = new Pelicula();
+
+           
+
+            int opc = 0;
+            do
+            {
+                Console.WriteLine("******************************");
+                Console.WriteLine("1 ver peliculas disponibles");
+                Console.WriteLine("2 verificar disponibilidad");
+                Console.WriteLine("3 comprar pelicula");
+                Console.WriteLine("4 devolver película");
+                Console.WriteLine("5 peliculas populares");
+                Console.WriteLine("6 Salir");
+                Console.WriteLine("******************************");
+                Console.WriteLine("Ingrese su Opción");
+                opc=int.Parse(Console.ReadLine());
+                
+                switch (opc)
+                {
+                    case 1:
+                        Console.WriteLine("opcion 1");
+                        peli.peliculaDisponible();
+                        break;
+                    case 2:
+                        Console.WriteLine("opcion 2");
+                        break;
+                    case 3:
+                        Console.WriteLine("opcion 3");
+                        break;
+                    case 4:
+                        Console.WriteLine("opcion 4");
+                        break;
+                    case 5:
+                        Console.WriteLine("opcion 5");
+                        break;
+                    default:
+                        Console.WriteLine("opcion equivocada");
+                        break;
+
+                }
+
+            } while (opc!=6);
+
+           
+        }
+        //private static void Ejecutar()
+        //{
+        //    Pelicula dato = new Pelicula();
+        //    char opcion = ' ';
+        //    do
+        //    {
+        //        Pelicula modelo = new Pelicula();
+        //        Console.WriteLine("Digite el ID dela Película");
+        //        modelo.Id = int.Parse(Console.ReadLine());
+
+        //        Console.WriteLine("Digite el título de la Película");
+        //        modelo.Titulo = Console.ReadLine();
+               
+        //        Console.WriteLine("Digite el género de la Película");
+        //        modelo.Genero = Console.ReadLine();
+
+        //        Console.WriteLine("Digite el precio de la Película");
+        //        modelo.Precio =double.Parse(Console.ReadLine());
+
+        //        Console.WriteLine("Digite el disponibilidad de la Película");
+        //        modelo.Disponibilidad = Console.ReadLine();
+
+        //        Console.WriteLine("Digite el puntuación de la Película");
+        //        modelo.Puntuacion = Console.ReadLine();
+
+        //        Console.WriteLine("Digite el estreno de la Película");
+        //        modelo.Estreno = int.Parse(Console.ReadLine());
+
+        //        dato.Guardar(modelo);;
+        //        Console.WriteLine("Desea añadir otrapelicula?");
+        //        opcion = char.Parse(Console.ReadLine());
+
+        //    } while (opcion.Equals('s') || opcion.Equals('S'));
+        //    foreach (Pelicula item in dato.Consultar())
+        //    {
+        //        Console.WriteLine("ID: " + item.Id);
+        //        Console.WriteLine("TÍTULO: " + item.Titulo);
+        //        Console.WriteLine("GÉNERO: " + item.Genero);
+        //        Console.WriteLine("PRECIO: " + item.Precio);
+        //        Console.WriteLine("DISPONIBILIDAD: " + item.Disponibilidad);
+        //        Console.WriteLine("PUNTUACIÓN: " + item.Puntuacion);
+        //        Console.WriteLine("ESTRENO: " + item.Estreno);
+        //        Console.WriteLine("**************************************************");
+        //    }
+        //    Console.ReadLine();
+        //}
+    }
+}
