9ce1dfe68058ec2cfc55c53ed6005dbbec25a80a
crisgon123 <crisgon123@gmail.com>
2019-09-12 22:00:28 -0500

    12/09/2019 T

---
+++ Semana16/Dia4/EntityFrameworkPrueba_6/EntityFrameworkPrueba_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFrameworkPrueba_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using crea un bloque que al terminar elimina de la memoria todo lo que habia en este
+            using(FACTURAEntities context=new FACTURAEntities())
+            {
+                // CONTEXT CONTROLAMTODAS LAS ENTIDADES DE LA BD
+                var clientes=context.Customers.ToList();
+                //EL METODO ToList() HACE UNA CONSULTA A LA BD
+                foreach(var cliente in clientes)
+                {
+                    Console.WriteLine(cliente.Name);
+                }
+            }
+        }
+    }
+}
