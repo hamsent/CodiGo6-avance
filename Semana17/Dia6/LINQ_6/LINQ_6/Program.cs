e0f7ae39a37254c2a69829b9970dae2b7cb6c797
crisgon123 <crisgon123@gmail.com>
2019-10-02 21:59:09 -0500

    02/10/2019 T

--- Semana17/Dia6/LINQ_6/LINQ_6/Program.cs
+++ Semana17/Dia6/LINQ_6/LINQ_6/Program.cs
@@ -10,13 +10,13 @@ namespace LINQ_6
     {
         static void Main(string[] args)
         {
-            using (BikeStoresEntities context=new BikeStoresEntities())
+            using (BikeStoresEntities context = new BikeStoresEntities())
             {
-                foreach(var categoria in context.categories.ToList())
+                foreach (var categoria in context.categories.ToList())
                 {
                     Console.WriteLine(categoria.category_name);
                 }
-                List<int> miLista = new List<int>{ 1,2,3,4,5,6,7,8};
+                List<int> miLista = new List<int> { 1, 2, 3, 4, 5, 6, 7, 8 };
                 var pares = miLista.FindAll(x => x % 2 == 0);
 
 
@@ -37,10 +37,10 @@ namespace LINQ_6
                 }
 
                 //CONSULTA CON LA BASE DE DATOS TODO EL PERSONAL (STAFFS) CUYO APELLIDO EMPIECE CON D.
-               // var primeraLetra = cadena.StartsWith("H");
-                var empiezaConD =  from personal in context.staffs.ToList()
-                            where personal.last_name.StartsWith("D")
-                            select personal;
+                // var primeraLetra = cadena.StartsWith("H");
+                var empiezaConD = from personal in context.staffs.ToList()
+                                  where personal.last_name.StartsWith("D")
+                                  select personal;
                 foreach (var item in empiezaConD)
                 {
                     Console.WriteLine(item.last_name + " " + item.first_name);
@@ -56,13 +56,66 @@ namespace LINQ_6
                 var misClientes = from cliente in context.customers.ToList()
                                   where cliente.first_name.StartsWith("J") && cliente.email.Contains("@aol.com") && cliente.city == "Houston"
                                   select cliente;
+                //sql: select * from customers where first_name like  'J%' and email like '%@aol.com' and city='longview'
+
+                //select city from customers
+                var ciudades = from c in context.customers.ToList()
+                               select new { Ciudad = c.city };
+                foreach (var ciudad in ciudades)
+                {
+                    Console.WriteLine(ciudad.Ciudad);
+                }
+
+                // MOSTRAR EL TELÉFONO Y LA TIENDA DEL PERSONAL (STAFFS)
+                var telefonoYTienda = from s in context.staffs.ToList()
+                                      select new
+                                      {
+                                          Telefono = s.phone,
+                                          Tienda = s.store_id
+                                      };
+
+                var telefonoYTiendaX = context.staffs
+                   .Select(x => new
+                   {
+                       Telefono = x.phone,
+                       Tienda = x.store_id
+                   });
+
+                var clientesOrdenados = from c in context.customers.ToList()
+                                        orderby c.first_name descending,
+                                        c.last_name ascending
+                                        select c;
+
+                var clientesOrdenadosX = context.customers.OrderByDescending(x => x.first_name).ThenBy(x => x.last_name);
+
+                var grupoPorEstado = from clientes in context.customers.ToList()
+                                     where clientes.last_name.StartsWith("J")
+                                     group clientes by clientes.state into grupo
+                                     select grupo;
+
+                //mostar grupos por estadoq
+                Console.WriteLine("Grupos de estado: ");
+                foreach (var grupos in grupoPorEstado)
+                {
+                    Console.WriteLine(grupos.Key);
+                    foreach (var cliente in grupos)
+                    {
+                        Console.WriteLine(cliente.last_name);
+                    }
+                }
+
+                var grupoPorEstadoX = context.customers.GroupBy(x => x.state);
+                var joinClienteOrden = from c in context.customers.ToList()
+                                       join o in context.orders.ToList()
+                                       on c.customer_id equals o.customer_id
+                                       select new
+                                       {
+                                           Cliente = c.first_name,
+                                           Orden = o.order_status
+                                       };
 
 
-
-
-
-
-
+                var clientX = context.orders.Find(1).order_items;
 
 
 
---
+++ Semana19/trabajo/trabajo/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabajo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using(BikeStoresEntities context=new BikeStoresEntities())
+            {
+
+                //1 MOSTRAR LOS PRODUCTOS CON MODELO DEL AÑO 2016
+
+                //var ejercicio1 = from p in context.products.ToList()
+                //                 where p.model_year == 2016
+                //                 select new
+                //                 {
+                //                     Nombre = p.product_name,
+                //                     Modelo = p.model_year
+                //                 };
+
+                //var ejercicio1x = context.products.Where(x => x.model_year == 2016).Select(x => new
+                //{
+                //    Nombre = x.product_name,
+                //    Modelo = x.model_year
+
+                //});
+                //foreach(var item in ejercicio1x)
+                //{
+                //    Console.WriteLine(item.Nombre + " " + item.Modelo);
+                //}
+
+
+                //MOSTRAR PRODUCTOS EN UNA SOLA COLUMNA CON SU RESPECTIVO PRECIO Y AÑO **
+
+                //var product = context.products.Select(x => new
+                //{
+                //    Nombre=x.product_name + "|" + x.list_price + "|"+x.model_year
+                //});
+
+                //foreach(var item in product)
+                //{
+                //    Console.WriteLine(item.Nombre);
+                //}
+
+
+                //3 MOSTRAR LOS PRODUCTOS CON MODELO PRECIOS 5499.99, 1549.99, 999.99
+
+                //var product = context.products.Where(x => x.list_price ==5499.99m || x.list_price == 1549.99m || x.list_price == 999.99m).Select(x => new
+                //{
+                //    Nombre = "NOMBRE: "+ x.product_name + " MODELO: " + x.model_year+ " PRECIO: " + x.list_price
+                //});
+
+                //foreach (var item in product)
+                //{
+                //    Console.WriteLine(item.Nombre);
+                //}
+
+                //4 MOSTRAR CUÁNTOS CLIENTES TIENEN UN MISMO CÓDIGO POSTAL
+
+
+                var product = context.products.Where().Select(x => new
+                       {
+                           Nombre = "NOMBRE: " + x.product_name + " MODELO: " + x.model_year + " PRECIO: " + x.list_price
+                       });
+
+                foreach (var item in product)
+                {
+                    Console.WriteLine(item.Nombre);
+                }
+            }
+        }
+    }
+}
